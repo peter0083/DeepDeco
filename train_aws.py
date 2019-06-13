5a1a3ae803b88f3d870e49d68f41756647d5f7b7
Peter Lin <peter.tingyao@gmail.com>
2019-06-13 12:05:39 -0700

    try to train GauGAN from py script

--- train_aws.py
+++ train_aws.py
@@ -9,6 +9,10 @@
 import os
 import boto3
 
+# Part 1
+# download data from S3
+
+
 # code adapted from https://stackoverflow.com/questions/49772151/boto3-download-folder-from-s3
 # and https://www.mydatahack.com/comprehensive-guide-to-download-files-from-s3-with-python/
 
@@ -19,7 +23,7 @@ def download_directory_from_s3(bucket_name,
     """
     :param bucket_name: string
     :param remote_directory_name: string
-    :return: None
+    :return: local_path
     """
 
     s3 = boto3.resource('s3')
@@ -29,7 +33,26 @@ def download_directory_from_s3(bucket_name,
             os.makedirs(os.path.dirname(key.key))
         bucket.download_file(key.key, key.key)
         print('Downloaded file with boto3 resource')
+    local_path = os.path.dirname(key.key)
+    return local_path
 
 
 download_directory_from_s3('gauganspade', 'datasets_mini')
 
+# Part 2-1
+# train Nvidia GauGAN
+
+# to be added: a way to programmatically find the directory then save files
+
+# for local training
+# train gaugan path: /Users/peterlin/DeepDeco/src/gaugan/train_gaugan.py
+# label path: /Users/peterlin/DeepDeco/datasets_mini copy/coco_stuff/val_label
+# image path: /Users/peterlin/DeepDeco/dataset/datasets_mini copy/coco_stuff/val_img
+# number of classes: 80 + 91 = 171
+
+print("training Nvidia GauGAN")
+bashCommand = 'python /Users/peterlin/DeepDeco/src/gaugan/train_gaugan.py --name "local-testrun" --dataset_mode ' \
+              'custom --label_dir "/Users/peterlin/DeepDeco/datasets_mini copy/coco_stuff/val_label" --image_dir ' \
+              '"/Users/peterlin/DeepDeco/dataset/datasets_mini copy/coco_stuff/val_img" --label_nc 171 '
+print("running bash command")
+os.system(bashCommand)
