2052d764d1830a6c7f450cfa429d758748555724
Peter Lin <peter.tingyao@gmail.com>
2019-06-13 12:43:36 -0700

    added gpu id argument to bash command

--- train_aws.py
+++ train_aws.py
@@ -52,7 +52,7 @@ download_directory_from_s3('gauganspade', 'datasets_mini')
 
 # full command python /Users/peterlin/DeepDeco/src/gaugan/train_gaugan.py --name "local-testrun" --dataset_mode ' \
 #               'custom --label_dir "/Users/peterlin/DeepDeco/datasets_mini copy/coco_stuff/val_label" --image_dir ' \
-#               '"/Users/peterlin/DeepDeco/datasets_mini copy/coco_stuff/val_img" --label_nc 171 --gpu_ids'
+#               '"/Users/peterlin/DeepDeco/datasets_mini copy/coco_stuff/val_img" --label_nc 171 --gpu_ids 1'
 
 
 # for AWS training
@@ -61,13 +61,13 @@ download_directory_from_s3('gauganspade', 'datasets_mini')
 # image path: /home/ubuntu/DeepDeco/datasets_mini copy/coco_stuff/val_img
 # full command python /home/ubuntu/DeepDeco/src/gaugan/train_gaugan.py --name "local-testrun" --dataset_mode ' \
 #               'custom --label_dir "/home/ubuntu/DeepDeco/datasets_mini copy/coco_stuff/val_label" --image_dir ' \
-#               '"/home/ubuntu/DeepDeco/datasets_mini copy/coco_stuff/val_img" --label_nc 171 --gpu_ids
+#               '"/home/ubuntu/DeepDeco/datasets_mini copy/coco_stuff/val_img" --label_nc 171 --gpu_ids 1'
 
 print("training Nvidia GauGAN")
 
 bashCommand = 'python /home/ubuntu/DeepDeco/src/gaugan/train_gaugan.py --name "local-testrun" --dataset_mode ' \
                'custom --label_dir "/home/ubuntu/DeepDeco/datasets_mini copy/coco_stuff/val_label" --image_dir ' \
-               '"/home/ubuntu/DeepDeco/datasets_mini copy/coco_stuff/val_img" --label_nc 171 --gpu_ids'
+               '"/home/ubuntu/DeepDeco/datasets_mini copy/coco_stuff/val_img" --label_nc 171 --gpu_ids 1'
 print("running bash command")
 
 os.system(bashCommand)
