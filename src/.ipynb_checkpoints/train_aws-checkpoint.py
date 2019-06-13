8947b9f1ddcf7634a9eef770200fdf3b58b3547a
Peter Lin <peter.tingyao@gmail.com>
2019-06-12 19:52:33 -0700

    adding boto3

--- src/.ipynb_checkpoints/train_aws-checkpoint.py
+++ src/.ipynb_checkpoints/train_aws-checkpoint.py
@@ -7,6 +7,9 @@ arg2 = sys.argv[2]
 arg3 = sys.argv[3]
 arg4 = sys.argv[4]
 
+# arg 1 is experiment name: ie. testrun
+# arg 2 is label_dir: ie. 
+
 subprocess.call(['python /home/ubuntu/DeepDeco/train_gaugan.py --name ', 
                  arg1, 
                  ' --dataset_mode custom --label_dir ', 
