c3e148c9685f27e02db065bd3d3e39d0b0ed3fdc
Peter Lin <peter.tingyao@gmail.com>
2019-06-14 00:55:44 -0700

    skimage argument error: change line break

--- src/attngan/code/miscc/utils.py
+++ src/attngan/code/miscc/utils.py
@@ -130,9 +130,7 @@ def build_super_images(real_imgs, captions, ixtoword,
             one_map = attn[j]
             if (vis_size // att_sze) > 1:
                 one_map = \
-                    skimage.transform.pyramid_expand(one_map, sigma=20,
-                                                     upscale=vis_size // att_sze,
-                                                     multichannel=True)
+                    skimage.transform.pyramid_expand(one_map, sigma=20, upscale=vis_size // att_sze, multichannel=True)
             row_beforeNorm.append(one_map)
             minV = one_map.min()
             maxV = one_map.max()
@@ -231,9 +229,7 @@ def build_super_images2(real_imgs, captions, cap_lens, ixtoword,
             one_map = one_map * mask
             if (vis_size // att_sze) > 1:
                 one_map = \
-                    skimage.transform.pyramid_expand(one_map, sigma=20,
-                                                     upscale=vis_size // att_sze,
-                                                     multichannel=True)
+                    skimage.transform.pyramid_expand(one_map, sigma=20, upscale=vis_size // att_sze, multichannel=True)
             minV = one_map.min()
             maxV = one_map.max()
             one_map = (one_map - minV) / (maxV - minV)
