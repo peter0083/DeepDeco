2ab82796f2937dec3fba48558a2f42a2a508c2ce
Peter Lin <peter.tingyao@gmail.com>
2019-06-23 07:33:28 -0700

    change file path

--- inference_aws.py
+++ inference_aws.py
@@ -108,10 +108,10 @@ download_directory_from_s3('gauganspade', 'datasets_mini')
 print("Fast deep photo style transfer inference")
 
 
-bashCommand40 = "python run_fpst.py --in-path /home/ubuntu/DeepDeco/ikea_vimle_series_sketch.jpg  --style-path " \
+bashCommand40 = "sudo python ftdeephoto/run_fpst.py --in-path /home/ubuntu/DeepDeco/ikea_vimle_series_sketch.jpg  --style-path " \
                 "/home/ubuntu/DeepDeco/jeans.jpeg --checkpoint-path checkpoints --out-path " \
                 "output/output_stylized_image.jpg --deeplab-path " \
-                "deeplab/models/deeplabv3_pascal_train_aug_2018_01_04.tar.gz --batch-size 4 --slow "
+                "deeplab/models/deeplabv3_pascal_train_aug_2018_01_04.tar.gz --batch-size 4 --slow"
 
 
 os.system(bashCommand40)
