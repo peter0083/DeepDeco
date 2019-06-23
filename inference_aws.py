5af0a0c0ea3bcac8e8dc1828b86d4d69c29e7ae1
Peter Lin <peter.tingyao@gmail.com>
2019-06-23 07:53:29 -0700

    trying running without sudo to fix filepath

--- inference_aws.py
+++ inference_aws.py
@@ -107,11 +107,11 @@ download_directory_from_s3('gauganspade', 'datasets_mini')
 
 print("Fast deep photo style transfer inference")
 
-bashCommand40 = "sudo python /home/ubuntu/DeepDeco/src/ftdeepphoto/run_fpst.py --in-path " \
-                "/home/ubuntu/DeepDeco/ikea_vimle_series_sketch.jpg  " \
+bashCommand40 = "python src/ftdeepphoto/run_fpst.py --in-path " \
+                "ikea_vimle_series_sketch.jpg  " \
                 "--style-path " \
-                "/home/ubuntu/DeepDeco/jeans.jpeg --checkpoint-path checkpoints --out-path " \
-                "/home/ubuntu/DeepDeco/output/output_stylized_image.jpg --deeplab-path " \
+                "jeans.jpeg --checkpoint-path checkpoints --out-path " \
+                "output/output_stylized_image.jpg --deeplab-path " \
                 "deeplab/models/deeplabv3_pascal_train_aug_2018_01_04.tar.gz --batch-size 4"
 
 os.system(bashCommand40)
