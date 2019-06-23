34934b73db183b32bd334743b764559af4aabd69
Peter Lin <peter.tingyao@gmail.com>
2019-06-22 17:31:07 -0700

    try furniture with context

--- inference_aws.py
+++ inference_aws.py
@@ -95,7 +95,7 @@ bashCommand34 = 'sudo python /home/ubuntu/DeepDeco/src/deeplab/demo.py single' \
                  ' -c /home/ubuntu/DeepDeco/src/deeplab/configs/coco.yaml' \
                  ' -m /home/ubuntu/DeepDeco/src/deeplab/data/models/coco/deeplabv1_resnet101/caffemodel' \
                  '/deeplabv1_resnet101-coco.pth' \
-                 ' -i /home/ubuntu/DeepDeco/502.954.72sketch.jpg'
+                 ' -i /home/ubuntu/DeepDeco/ikea_vimle_series_sketch.jpg'
 
 os.system(bashCommand34)
 
