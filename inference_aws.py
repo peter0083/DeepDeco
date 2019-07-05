1db34328355134a18b507423d044e4d455caeb1b
Peter Lin <peter.tingyao@gmail.com>
2019-07-04 23:04:01 -0700

    updating inference script

--- inference_aws.py
+++ inference_aws.py
@@ -1,26 +1,9 @@
 ##############################################
 # this script runs inference
 # to generate an image based on
-# the following inputs
-# 1. a text description of the object (MS AttnGAN)
-# 2. a outline image of an object (Nvidia GauGAN)
-
-# to run this script on AWS Linux Set up the data folder to contain four things:
-#
-# 'train' folder. This contains 'filenames.pickle' for the training files 'test' folder. This should contain
-#  'filenames.pickle' for the test files
-#
-# 'coco' folder. This should contain all the images from the coco dataset. (train, val and test)
-#
-# 'captions.pickle' (if you plan to use pre-trained models)
-#  Update your configuration files in 'code/cfg/' to point to the parent
-#
-# directory of the 'coco' folder. E.g: if 'coco' folder is in 'data/coco/coco' path, put the 'data' path in your
-# config files as 'data/coco'
-#
-# ensure that `sudo python -V` gives your python 3.6
-#
-# ensure that `sudo conda install --file requirements.txt` is successful then execute "sudo python inference_aws.py"
+# the following inputs:
+# 1. an image of a piece of furniture (sketch)
+# 2. a string describing the desired text
 # #############################################
 
 import os
@@ -54,56 +37,9 @@ def download_directory_from_s3(bucket_name,
     return local_path
 
 
-download_directory_from_s3('gauganspade', 'datasets_mini')
 
-# need a separate command to download vgg19 weight from S3
-
-
-# add download for attnGAN in WK3
 
 # Part 2
-# image similarity search
-
-# Part 3
-# Deep Lab
-
-# print("DeepLab V2 segmentation: setup conda environment")
-#
-# bashCommand30 = 'conda env create -f configs/conda_env.yaml'
-#
-# os.system(bashCommand30)
-#
-# bashCommand31 = 'conda activate deeplabv2-pytorch'
-#
-# os.system(bashCommand31)
-#
-# print("DeepLab V2 segmentation: download caffemodel pre-trained on ImageNet and 91-class COCO (1GB+)")
-#
-# bashCommand32 = 'sudo bash /home/ubuntu/DeepDeco/src/deeplabv2/scripts/setup_caffemodels.sh'
-#
-# os.system(bashCommand32)
-#
-# print("DeepLab V2 segmentation: Convert the caffemodel to pytorch compatible.")
-#
-# bashCommand33 = 'scd udo python /home/ubuntu/DeepDeco/src/deeplabv2/convert.py --dataset coco'
-#
-# os.system(bashCommand33)
-#
-# print("DeepLab V2 segmentation: now segmenting image....")
-#
-# bashCommand34 = 'sudo python /home/ubuntu/DeepDeco/src/deeplabv2/demo.py single' \
-#                  ' -c /home/ubuntu/DeepDeco/src/deeplabv2/configs/coco.yaml' \
-#                  ' -m /home/ubuntu/DeepDeco/src/deeplabv2/data/models/coco/deeplabv1_resnet101/caffemodel' \
-#                  '/deeplabv1_resnet101-coco.pth' \
-#                  ' -i /home/ubuntu/DeepDeco/jeans.jpg'
-#
-# os.system(bashCommand34)
-#
-# print("DeepLab V2 segmentation: completed")
-
-
-
-# Part 4
 # Fast deep photo style transfer
 
 print("Fast deep photo style transfer inference")
