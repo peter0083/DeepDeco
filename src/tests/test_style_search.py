7f4cc84f6066b8557aa9ddbe1af349fc4cae22f0
Peter Lin <peter.tingyao@gmail.com>
2019-07-08 09:01:15 -0700

    added new test to style search engine

--- src/tests/test_style_search.py
+++ src/tests/test_style_search.py
@@ -17,3 +17,16 @@ def test_input_text():
         subprocess.run(run_engine_command)
 
 
+def test_input_image():
+    """
+    This function checks string input type for image file path
+    The Find-Max-Similarity function should raise a TypeError
+    """
+
+    run_engine_command = ["python", "src/stylesearch/run_engine.py", "--input",
+                          "patterned glass", "--content", 123]
+
+    with pytest.raises(TypeError):
+        subprocess.run(run_engine_command)
+
+
